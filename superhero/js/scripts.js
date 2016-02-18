c82c8b83d0940b59c22c6b5b0472f65997313300
sreadbrown <student@epicodus.com>
2016-02-17 20:11:16 -0800

    add basic html elements and pictures for each character

---
+++ superhero/js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
+
+// OLD CODE PRE LOOPING SAVED FOR REFERENCE
+// $(document).ready(function() {
+//   $("#blanks form").submit(function(event) {
+//     var person1Input = $("input#person1").val().toUpperCase();
+//     var person2Input = $("input#person2").val().toUpperCase();
+//     var animalInput = $("input#animal").val().toUpperCase();
+//     var exclamationInput = $("input#exclamation").val();
+//     var verbInput = $("input#verb").val();
+//     var nounInput = $("input#noun").val();
+//
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+//
+//     $("#story").show();
+//
+//     event.preventDefault();
+//   });
+// });
