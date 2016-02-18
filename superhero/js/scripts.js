b8d3a82b0e06e91064b60487ea8daf4402fda7fa
sreadbrown <student@epicodus.com>
2016-02-17 21:02:48 -0800

    a comment about how to improve efficiency of if and else if statements

--- superhero/js/scripts.js
+++ superhero/js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         if (power === "Flight") {
         $('#storm').show();
         }
-
+// move on to else if 
         if (power === "Strength") {
         $('#jubilee').show();
         }
